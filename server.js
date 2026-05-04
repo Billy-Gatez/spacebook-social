bf758ddd84b60d1206b8ba4384b441f418186875
Billy-Gatez <JMCAHILL@STUDENT.FULLSAIL.EDU>
2026-05-04 07:24:41 -0700

    s

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const ageCheckRouter = require("./routes/ageCheck");
+const ageCheckRouter = require("../routes/ageCheck");
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const path = require("path");
