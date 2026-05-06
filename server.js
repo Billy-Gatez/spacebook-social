8ec5b386a54810ff481ad08c09084d85637cc715
Billy-Gatez <JMCAHILL@STUDENT.FULLSAIL.EDU>
2026-05-05 19:11:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ const tetrixScoreSchema = new mongoose.Schema({
   avatar:   { type: String, default: '🎮' },
   title:    { type: String, default: '' },
   score:    { type: Number, required: true },
-  mode:     { type: String, default: 'Online VS' },
+  mode:     { type: String, default: 'Classic' },
   createdAt:{ type: Date,   default: Date.now }
 });
 
